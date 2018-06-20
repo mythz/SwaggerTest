be3a884fcf203e2fe3aa2508a1cdaf3999b94377
Demis Bellot <demis.bellot@gmail.com>
2018-06-20 07:10:07 -0400

    Add SwaggerTest ASP.NET Core v4.6.1 project with OpenApiFeature

---
+++ SwaggerTest.ServiceInterface/MyServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ServiceStack;
+using SwaggerTest.ServiceModel;
+
+namespace SwaggerTest.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
